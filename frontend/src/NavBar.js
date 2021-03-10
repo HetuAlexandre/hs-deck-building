887f082fbbe12cad3408a1c1fc4edd39a626067e
“Alexandre <Alex_18h@hotmail.com>
2021-03-09 20:09:26 -0500

    navbar

---
+++ frontend/src/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const NavBar =()=>{
+    return(
+        <Wrapper>
+            <Home>
+                HEARTHSTONE DECK BUILDER
+            </Home>
+                <Container>
+                    <News></News>
+                    <Cards></Cards>
+                    <Profile></Profile>
+                </Container>
+
+        </Wrapper>
+    )
+}
