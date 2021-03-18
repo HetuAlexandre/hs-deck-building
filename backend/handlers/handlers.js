d8866583cbfa077a4166749f98b725a1e42e2b3b
“Alexandre <Alex_18h@hotmail.com>
2021-03-18 13:36:04 -0400

    add get all card and get card detail in handlers

--- backend/handlers/handlers.js
+++ backend/handlers/handlers.js
@@ -1,11 +1,34 @@
-const getOneCard = (req, res) => {
+//Return all cards
+//res={cards: Array(40), cardCount: 3092, pageCount: 78, page: 1}
+const getCards = (req, res) => {
+  console.log(cards, "ALL CARDS");
   res.status(200).json({
     status: 200,
-    message: "request for one card fulfilled",
-    data: {},
+    message: "Request for getting all cards success",
+    data: { ...cards },
   });
 };
 
+//Return one card
+
+const getCardDetail = (req, res) => {
+  let card = cards.find((card) => card.id == req.params.id);
+  console.log(card, "ONE CARD");
+  if (card) {
+    res.status(200).json({
+      status: 200,
+      data: { ...cards },
+      message: "Request for getting one card success",
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      error: "card not found",
+    });
+  }
+};
+
 module.exports = {
-  getOneCard,
+  getCards,
+  getCardDetail,
 };
