d833fcec13330f8c4402cacdc86f5103bc2788b6
“Alexandre <Alex_18h@hotmail.com>
2021-04-05 13:54:57 -0400

    filter non hero and styling

--- frontend/src/pages/Cards.js
+++ frontend/src/pages/Cards.js
@@ -1,38 +1,97 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
-// import ReactPaginate from "react-paginate";
 import { Link } from "react-router-dom";
 import { CardsContext } from "../Provider/CardsContext";
-// import Pagination from "../components/Pagination";
-// , pageCount, setCurrentPage
+
 const Cards = () => {
   const { cards, cardsStatus } = useContext(CardsContext);
-
+  const [searchCards, setSearchCards] = useState("");
+  const nonHerosCards = cards.filter((card) => card.cardTypeId !== 3);
+  console.log(cards, "CARDS");
   if (cardsStatus === "loading") {
     return <div>Loading...</div>;
   } else {
     return (
       <Wrapper>
-        {cards.map((card) => {
-          if (card.artistName != null)
+        <Search>
+          <Input
+            type="search"
+            placeholder="Search..."
+            placeholderTextColor="white"
+            onChange={(event) => {
+              setSearchCards(event.target.value);
+            }}
+          />
+        </Search>
+        {nonHerosCards
+          .filter((card) => {
+            if (searchCards == "") {
+              return card;
+            } else if (
+              card.name.toLowerCase().includes(searchCards.toLocaleLowerCase())
+            )
+              return card;
+          })
+          .map((card, key) => {
             return (
-              <CardContainer>
-                <CardDiv to={`/cardDetail/${card.id}`}>
-                  <Img src={card.image}></Img>
-                </CardDiv>
-              </CardContainer>
+              <Container>
+                <CardContainer>
+                  <CardDiv to={`/cardDetail/${card.id}`}>
+                    <Img src={card.image}></Img>
+                  </CardDiv>
+                </CardContainer>
+              </Container>
             );
-        })}
+          })}
       </Wrapper>
     );
   }
 };
 const Wrapper = styled.div`
+  background-color: #fff5d0;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+  margin: 0;
 `;
-const CardContainer = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 0;
+`;
+const CardContainer = styled.div`
+  display: flex;
+`;
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 50px;
+  background-color: darkred;
+  border-top: 3px solid #c92626;
+  border-bottom: 3px solid #c92626;
+`;
+const Input = styled.input`
+  width: 250px;
+  height: 30px;
+  font-size: 18px;
+  padding-left: 30px;
+  color: lightgray;
+  font-weight: 900;
+  border: 3px solid rgb(252, 209, 68);
+  border-radius: 20px;
+  background-color: darkred;
+  &:hover {
+    box-shadow: 0 0 30px rgb(252, 209, 68);
+    border: 3px solid rgb(252, 209, 68);
+  }
+  &::-webkit-input-placeholder {
+    color: lightgray;
+    /* text-decoration: none; */
+  }
+`;
 
 const CardDiv = styled(Link)`
   margin-bottom: 20px;
@@ -44,6 +103,17 @@ const Img = styled.img`
 `;
 
 export default Cards;
+
+//highlight blue
+// &:hover {
+//   box-shadow: 0 0 30px rgba(81, 203, 238, 1);
+//   border: 3px solid rgba(81, 203, 238, 1);
+// }
+
+// box-shadow: 0 0 5px rgba(81, 203, 238, 1);
+//   padding: 3px 0px 3px 3px;
+//   margin: 5px 1px 3px 0px;
+//   border: 1px solid rgba(81, 203, 238, 1);
 
 // const Pagination = styled.div`
 //   width: 100%;
