9f2d34839e4ac91af3818c10fc0f5b5154d9f9a0
“Alexandre <Alex_18h@hotmail.com>
2021-04-05 13:47:43 -0400

    made change / styling to nav bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-
 const NavBar = () => {
   return (
     <Wrapper>
@@ -10,10 +9,10 @@ const NavBar = () => {
           <span>HEARTHSTONE</span> DECK BUILDER
         </Home>
         <ButtonContainer>
-          <ButtonNav to={"/news"}>NEWS</ButtonNav>
+          {/* <ButtonNav to={"/news"}>NEWS</ButtonNav> */}
           <ButtonNav to={"/cards"}>CARDS</ButtonNav>
-          <ButtonNav to={"/deckbuilders"}>DECK BUILDERS</ButtonNav>
-          <ButtonNav to={"/decks"}>DECKS</ButtonNav>
+          <ButtonNav to={"/selectClasses"}>DECK BUILDERS</ButtonNav>
+          <ButtonNav to={"/premadeDecks"}>DECKS</ButtonNav>
           <ButtonNav to={"/profile"}>PROFILE</ButtonNav>
         </ButtonContainer>
       </NavWrapper>
@@ -22,42 +21,57 @@ const NavBar = () => {
 };
 const Wrapper = styled.div`
   width: 100%;
-
-  border: 2px solid pink;
+  background-color: rgba(21, 26, 35, 0.95);
 `;
 
 const NavWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
   width: 100%;
   height: 100px;
-
-  border: 2px solid black;
+  /* margin-bottom: 20px; */
+  text-decoration: none;
 `;
 const Home = styled(Link)`
+  color: lightgray;
   width: 260px;
-  border: 2px solid red;
   margin: 10px 40px 0 20px;
   font-size: 25px;
   text-decoration: none;
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
   span {
+    color: rgba(81, 203, 238, 1);
     font-weight: bold;
     font-size: 35px;
   }
 `;
 const ButtonContainer = styled.div`
+  width: 100%;
   display: flex;
-  justify-content: center;
+  /* justify-content: center; */
   align-items: center;
-  border: 2px solid green;
 `;
 const ButtonNav = styled(Link)`
+  color: rgb(252, 209, 68);
   display: flex;
   justify-content: center;
-  width: 200px;
+  width: 160px;
   font-size: 20px;
-  border: 2px solid blue;
   text-decoration: none;
+  &:focus,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+  &:hover {
+    color: rgb(255, 240, 191);
+  }
 `;
 
 export default NavBar;
