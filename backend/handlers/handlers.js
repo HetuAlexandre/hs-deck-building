a1cac52d53e9a10b7795294986af44c1b0cf3e7f
“Alexandre <Alex_18h@hotmail.com>
2021-04-09 15:24:36 -0400

    mongo add

--- backend/handlers/handlers.js
+++ backend/handlers/handlers.js
@@ -1,34 +1,126 @@
-//Return all cards
-//res={cards: Array(40), cardCount: 3092, pageCount: 78, page: 1}
-const getCards = (req, res) => {
-  console.log(cards, "ALL CARDS");
-  res.status(200).json({
-    status: 200,
-    message: "Request for getting all cards success",
-    data: { ...cards },
-  });
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const assert = require("assert");
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
-//Return one card
+const getUser = async (req, res) => {
+  const { userId } = req.body;
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+    const db = client.db("hsdatabase");
+    const user = await db.collection("users").findOne({ _id: userId });
+    client.close();
+    if (!user) {
+      return res.status(404).json({
+        status: 404,
+        error: "User not found.",
+      });
+    }
 
-const getCardDetail = (req, res) => {
-  let card = cards.find((card) => card.id == req.params.id);
-  console.log(card, "ONE CARD");
-  if (card) {
-    res.status(200).json({
+    return res.status(200).json({
       status: 200,
-      data: { ...cards },
-      message: "Request for getting one card success",
+      userId: user._id,
+      user,
     });
-  } else {
-    res.status(404).json({
-      status: 404,
-      error: "card not found",
+  } catch (err) {
+    console.log(err.stack);
+    client.close();
+
+    return res.status(500).json({
+      status: 500,
+      error: err,
+    });
+  }
+};
+
+const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  const salt = await bcrypt.genSalt();
+  const hashPassword = await bcrypt.hash(password, salt);
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+    const db = client.db("hsdatabase");
+    const user = {
+      _id: uuidv4(),
+      name,
+      email,
+      password: hashPassword,
+    };
+    const result = await db.collection("users").insertOne(user);
+    assert.equal(1, result.insertedCount);
+    client.close();
+    if (result.insertedCount === 1) {
+      return res.status(201).json({
+        status: 201,
+        message: "User added.",
+        user,
+      });
+    } else {
+      return res.status(400).json({
+        status: 400,
+        error: "Input not accepted.",
+      });
+    }
+  } catch (err) {
+    console.log(err.stack);
+    client.close();
+    return res.status(500).json({
+      status: 500,
+      error: err,
+    });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+    const db = client.db("hsdatabase");
+    const user = await db.collection("users").findOne({ email });
+    client.close();
+    if (!user) {
+      return res.status(404).json({
+        status: 404,
+        error: "Sign up first.",
+      });
+    }
+    const passwordIsValidated = await bcrypt.compare(password, user.password);
+    if (!passwordIsValidated) {
+      return res.status(401).json({
+        status: 401,
+        error: "Password is incorrect.",
+      });
+    } else {
+      return res.status(200).json({
+        status: 200,
+        message: "Logged in",
+        userId: user._id,
+        user,
+      });
+    }
+  } catch (err) {
+    console.log(err.stack);
+    client.close();
+    return res.status(500).json({
+      status: 500,
+      error: err,
     });
   }
 };
 
 module.exports = {
-  getCards,
-  getCardDetail,
+  login,
+  signup,
+  getUser,
 };
