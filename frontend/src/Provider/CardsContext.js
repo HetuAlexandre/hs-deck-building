3a6b550116b622fab8968ab1bf75f8033a2b96e1
“Alexandre <Alex_18h@hotmail.com>
2021-04-09 15:29:34 -0400

    clean up

--- frontend/src/Provider/CardsContext.js
+++ frontend/src/Provider/CardsContext.js
@@ -6,9 +6,6 @@ export const CardsContext = createContext(null);
 
 export const CardsProvider = ({ children }) => {
   const [cards, setCards] = useState(null);
-  // const [metaCards, setMetaCards] = useState(null);
-  // const [pageCount, setPageCount] = useState(0);
-  // const [currentPage, setCurrentPage] = useState(1);
   const [cardsStatus, setCardsStatus] = useState("loading");
 
   // Cards
@@ -20,7 +17,6 @@ export const CardsProvider = ({ children }) => {
       .then((res) => res.json())
       .then((res) => {
         setCards(Object.values(res.cards));
-        // setPageCount(res.pageCount);
         console.log(res.cards, "response cards");
         setCardsStatus("idle");
       })
@@ -29,32 +25,11 @@ export const CardsProvider = ({ children }) => {
         setCardsStatus("error");
       });
   }, []);
-  //Metadata Cards
-  // useEffect(() => {
-  //   setCardsStatus("loading");
-  //   fetch(
-  //     `https://us.api.blizzard.com/hearthstone/metadata?locale=en_US&access_token=${accessToken}`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       setMetaCards(Object.values(res.metaCards));
-  //       // setPageCount(res.pageCount);
-  //       setCardsStatus("idle");
-  //     })
-  //     .catch((error) => {
-  //       console.error("Unable to retrieve metadata cards", error);
-  //       setCardsStatus("error");
-  //     });
-  // }, []);
   return (
     <CardsContext.Provider
       value={{
         cards,
         cardsStatus,
-        // metaCards,
-        // pageCount,
-        // currentPage,
-        // setCurrentPage,
       }}
     >
       {children}
